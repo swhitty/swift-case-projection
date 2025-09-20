48c84e2bb849c2b47d046e2d36b39b40a5adbe7e
Simon Whitty <simon@whileloop.com>
2025-09-20 16:38:30 +1000

    swift-syntax 602.0.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax", "510.0.0"..<"602.0.0")
+        .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"603.0.0")
     ],
     targets: [
         .target(
